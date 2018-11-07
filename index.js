ba19c5c80c3e352e823230cd1af3979895413974
drakeltheryuujin <kellye.greene@flatironschool.com>
2018-11-07 17:35:20 -0500

    UPdates code

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 function greet(time) {
   var greet;
 
-  if (time < 12)
+  if (time < 12) {
       greet = 'Good Morning';
-  else if (time >= 12 && hrs <= 17)
+  } else if (time >= 12 && time <= 17) {
       greet = 'Good Afternoon';
-  else if (time >= 17 && hrs <= 24)
+  } else if (time >= 17 && time <= 24) {
       greet = 'Good Evening';
+  } else {
+    greet = 'Hello';
+  }
+  document.getElementById("greeting").innerHTML = greet;
 }
+
